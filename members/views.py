9dae322a91a52c1211d3047f5e9bb4aec2ec54c3
Rudreshcg <108670628+Rudreshcg@users.noreply.github.com>
2022-07-12 18:54:45 +0530

    Add files via upload

---
+++ members/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.template import loader
+from .models import Members
+
+# Create your views here.
+def index(request):
+   mymembers =Members.objects.all().values()
+   template = loader.get_template('index.html')
+   context = {
+       'mymembers':mymembers,
+   }
+   return HttpResponse(template.render(context,request))
+
+def add(request):
+   templete = loader.get_template('add.html')
+   return HttpResponse(templete.render({}, request))
+
+def addrecord(request):
+   x = request.POST['first']
+   y = request.POST['last']
+   member = Members(firstname=x,lastname=y)
+   member.save()
+   return HttpResponseRedirect(reverse('index'))
+
